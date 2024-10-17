a81a5a26332b865c40e9f33d4881ce82d8fbcf86
Ginny-77 <duzijing0701@163.com>
2024-10-17 15:28:59 +0800

    test

--- 1_code/1_data_preparation/1_wearable_data.R
+++ 1_code/1_data_preparation/1_wearable_data.R
@@ -4,6 +4,14 @@ rm(list = ls())
 source('1_code/100_tools.R')
 
 ###read data
+data2 <-
+  readxl::read_xlsx(
+    "2_data/wearable data/",
+    sheet = 1
+  )
 
+
+###
 dir.create()
 setwd()
+ ##test
